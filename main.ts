561e60d77bee28a8ec088ebaaf2a4af4f39058e7
Gregório Patriota Correia <gregorio.correia@pe.senai.br>
2019-09-01 22:10:26 -0300

    EPNT : aplicando conhecimentos aprendidos

---
+++ main.ts
@@ -0,0 +1,17 @@
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name: 'e-dot-api',
+    version: '0.1.0'
+})
+
+server.get('/hello', (req, res, next) => {
+    res.json({
+        message: 'hello'
+    })
+    return next()
+})
+
+server.listen(3000, ()=>{
+    console.log('API running on port 3000')
+})
